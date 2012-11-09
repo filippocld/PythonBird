18ea7708837727de2d153a10dc3141c8a0e359c0
Diego Pignattini <pignattini.digone.diego@gmail.com>
2012-11-09 17:04:32 +0100

    Compatibility Update

--- pythonbird.py
+++ pythonbird.py
@@ -19,9 +19,9 @@ myself = api.me()
 
 #This function gets the last 1000 tweets given a user
 def getTweets():
-	print 'Write the name of the user you want to get tweets from'
+	print ('Write the name of the user you want to get tweets from')
 	user = raw_input()
-	print 'Getting tweets...'
+	print ('Getting tweets...')
 	tweets = []
 	for result in tweepy.cursor.Cursor(api.user_timeline,include_rts=True,screen_name=user).items():
 		tweets = tweets + [result]
@@ -54,17 +54,17 @@ def getFollowers(userID):
 def matchFollowers(user,otherUser=myself):
 	
 	#First user's followings
-	print 'Getting',user.screen_name,'followings...'
+	print ('Getting',user.screen_name,'followings...')
 	follo1 = getFollowing(user.id)
 	
 	#Then otherUser's followers
-	print 'Getting',otherUser.screen_name,'followers...'
+	print ('Getting',otherUser.screen_name,'followers...')
 	follo2 = getFollowers(otherUser.id)
 	
 	common = []
 	unCommon = []
 	
-	print 'Ckecking lists...'
+	print ('Ckecking lists...')
 	
 	#Common followers/following
 	for f1 in follo1:
@@ -94,7 +94,7 @@ def defollow(user):
 
 #Shows people who don't follow you and asks for unfollowing 
 def checkMatch():
-	print 'Wait for followers/following scan \n'
+	print ('Wait for followers/following scan \n')
 	matches = matchFollowers(myself)
 	
 	goodList = []
@@ -107,43 +107,43 @@ def checkMatch():
 		badList += [match.screen_name]
 	
 	
-	print '\n'
-	print 'These are',myself.screen_name,"'s followings who followback \n"
+	print ()
+	print ('These are',myself.screen_name,"'s followings who followback \n")
 	for i in goodList:
-		print i
+		print (i)
 	
-	print '\n'
-	print 'These are',myself.screen_name,"'s followings who don't followback \n"
+	print ()
+	print ('These are',myself.screen_name,"'s followings who don't followback \n")
 	for i in badList:
-		print i
+		print (i)
 	
 	for i in matches[1]:
-		print ''
-		print 'Do you want to unfollow', i.screen_name,'?'
-		print 'Type "u" to unfollow, "x" to exit, any other to ignore'
+		print ()
+		print ('Do you want to unfollow', i.screen_name,'?')
+		print ('Type "u" to unfollow, "x" to exit, any other to ignore')
 		answer = raw_input()
 		if answer == 'd':
 			defollow(i)
-			print i.screen_name,' has been unfollowed \n'
+			print (i.screen_name,' has been unfollowed \n')
 		elif answer=='x':
 			break
 		else:
-			print 'Next... \n'
+			print ('Next... \n')
 			
-	print 'All operations done'			
+	print ('All operations done')			
 
 
 #Sends a DM to all your followers
 def massDM():
 	#It gets all your followers, first.
-	print 'Searching followers...'
+	print ('Searching followers...')
 	x = getFollowers(myself.id)
-	print 'You have '+str(len(x))+' followers'
-	print 'Write your DM here (max 140 chars)'
+	print ('You have '+str(len(x))+' followers')
+	print ('Write your DM here (max 140 chars)')
 	dmText = raw_input()
 	if len(dmText) > 140:
-		print 'You talk too much'
-		print 'Write your DM again'
+		print ('You talk too much')
+		print ('Write your DM again')
 		dmText = raw_input()
 		
 	countSent = 0 #Sent DMs
@@ -159,8 +159,8 @@ def massDM():
 			countUnsent += 1 #If anyone unfollow you during the process
 		sleep(1)
 		
-	print '\n'+str(countSent)+' DMs have been sent'
-	print 'PythonBird could not send '+str(countUnsent)+' DMs\n'
+	print ('\n'+str(countSent)+' DMs have been sent')
+	print ('PythonBird could not send '+str(countUnsent)+' DMs\n')
 	return
 	
 
@@ -173,7 +173,7 @@ def fileSaveTweets(tweetsList):
 	thisPath = path.dirname(thisPath)
 	tweetsFile = open('tweets.txt','w') #Create a file
 	tweets = []
-	print 'Writing file...\n'
+	print ('Writing file...\n')
 	errors = 0
 	for tweet in tweetsList:
 		try:
@@ -185,26 +185,26 @@ def fileSaveTweets(tweetsList):
 	tweetsFile.write('===============================\n')
 	tweetsFile.writelines(tweets)
 	tweetsFile.close()
-	print 'PB has been unable to save '+str(errors)+' tweets\n'
-	print str(lenght-errors)+' tweets have been saved\n'
-	print 'File has been saved in '+str(thisPath)+'/tweet.txt\n'
+	print ('PB has been unable to save '+str(errors)+' tweets\n')
+	print (str(lenght-errors)+' tweets have been saved\n')
+	print ('File has been saved in '+str(thisPath)+'/tweet.txt\n')
 	return 	
 
 def begin():
 	
 	begin = 'R'
-	print '\n'
-	print '=================================================='
-	print 'WELCOME TO PYTHONBIRD, A PYTHON-SHELL TWITTER TOOL'
-	print '=================================================='
-	print '\n'
-	print 'Hello', myself.screen_name
-	print '______________________________________________'
+	print ()
+	print ('==================================================')
+	print ('WELCOME TO PYTHONBIRD, A PYTHON-SHELL TWITTER TOOL')
+	print ('==================================================')
+	print ()
+	print ('Hello', myself.screen_name)
+	print ('______________________________________________')
 	while(begin is 'R'):
 		print 'Do you want to unfollow (u), mass-DM (m) or save tweets(t)?'
 		answer = raw_input()
 		while (not ((answer is 'u') or (answer is 'm') or (answer is 't'))):
-			print 'Bad choice, try again: u = unfollow, m = mass-DM, t = save tweets'
+			print ('Bad choice, try again: u = unfollow, m = mass-DM, t = save tweets')
 			answer = raw_input()
 	
 		if answer is 'u':
@@ -216,7 +216,7 @@ def begin():
 			fileSaveTweets(timeline)
 			
 		
-		print 'Type R to restart, X to exit'
+		print ('Type R to restart, X to exit')
 		begin = raw_input()
 	return
 		
